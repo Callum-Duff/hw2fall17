fd2a4e2724c8d5cc16cca9122e6964a718557d60
Callum Duff <calduff@uiowa.edu>
2017-09-12 09:10:38 -0500

    Worked on part 4, but the array won't print the initial value

--- lib/part4.rb
+++ lib/part4.rb
@@ -4,7 +4,41 @@ class Class
     attr_name = attr_name.to_s   # make sure it's a string
     attr_reader attr_name        # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-    class_eval "YOUR CODE HERE, USE %Q FOR MULTILINE STRINGS"
+    attr_name_history = attr_name.to_s + '_history'
+
+    class_eval %Q"
+      def #{attr_name}= newValue
+        if defined? @#{attr_name_history}
+          @#{attr_name_history} << newValue
+        else
+          @#{attr_name_history} = [newValue]
+        end
+        @#{attr_name} = newValue
+      end
+    "
 
   end
 end
+
+class TestClass
+  attr_accessor_with_history :value
+
+  def initialize(value)
+    @value = value
+  end
+
+
+
+  def printHistory
+    puts @value_history
+  end
+
+end
+
+
+example = TestClass.new(5)
+example.value = 12
+example.value = 25
+example.value = 14
+puts 'Printing history:'
+example.printHistory
